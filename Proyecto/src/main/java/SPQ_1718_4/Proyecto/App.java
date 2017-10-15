25184ef71be0bfef1d0e600cbeff5108038b824f
asierrodri28 <asier.rodriguez@opendeusto.es>
2017-10-15 19:36:43 +0200

    Creación de los paquetes para empezar el desarrollo #DSPQ1718P4-4

---
+++ Proyecto/src/main/java/SPQ_1718_4/Proyecto/App.java
@@ -0,0 +1,13 @@
+package SPQ_1718_4.Proyecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Proyecto/src/main/java/Visualizacion_DashBoard/Ventana_DashBoard.java
@@ -0,0 +1,15 @@
+package Visualizacion_DashBoard;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+public class Ventana_DashBoard extends JFrame {
+	public JTable tablaPrincipal;
+	public JButton crearTorneo;
+	public JButton administrarTorneo;
+	public Ventana_DashBoard() {
+		super();
+	}
+}
