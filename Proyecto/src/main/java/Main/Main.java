bb9867223628f5482fdab80055487a772616fbde
asierrodri28 <asier.rodriguez@opendeusto.es>
2017-10-16 12:32:06 +0200

    No funciona log4j

--- Proyecto/src/main/java/Main/Main.java
+++ Proyecto/src/main/java/Main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BasicConfigurator.configure();
+		//BasicConfigurator.configure();
 		Logger log=Logger.getLogger(Main.class);
 		log.debug("Funciona");
 	}
