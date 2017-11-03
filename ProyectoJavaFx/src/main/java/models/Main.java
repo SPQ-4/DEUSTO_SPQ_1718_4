d1c268b65f463155917a586f8e8d93efd58a0bf0
Juan Garayalde <juangara@opendeusto.es>
2017-11-03 20:25:01 +0100

    Primeros pasos JAVAFX

--- ProyectoJavaFx/src/main/java/models/Main.java
+++ ProyectoJavaFx/src/main/java/models/Main.java
@@ -1,17 +1,21 @@
 package models;
 
+import java.util.Stack;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/views/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../views/sample.fxml"));
         primaryStage.setTitle("Hello World");
+        StackPane frame = new StackPane();
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
