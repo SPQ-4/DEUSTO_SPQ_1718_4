1d0fa72d57930d4d0d740e8dc727795d74d1de1b
Paula.Urain <paulaue@opendeusto.es>
2017-11-04 16:24:34 +0100

    query hecha por cada tipo torneo, falta definir los tipos de torneo #DSPQ1718P4-25 add work 45m

--- ProyectoJavaFx/src/main/java/controllers/ControllerGeneral.java
+++ ProyectoJavaFx/src/main/java/controllers/ControllerGeneral.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import db.MySQLDriver;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -28,8 +31,13 @@ public class ControllerGeneral {
 	@FXML
 	private Pane centralPane;
 	
+	private MySQLDriver driverDB;
+	
 	@FXML
 	public void initialize() {
+
+		driverDB= new MySQLDriver();
+		//todos los setOnAction es para darles funcionalidad a los botones del menú de navegación
 		actualState.setOnAction(new EventHandler<ActionEvent>() {
     	    @Override public void handle(ActionEvent e) {
     	    	centralPane.getChildren().clear();
@@ -69,8 +77,28 @@ public class ControllerGeneral {
     	    @Override public void handle(ActionEvent e) {
     	    	Stage stage = (Stage) exit.getScene().getWindow();
     	        // do what you have to do
-    	        stage.close();
+    	       // stage.close();
+    	    	try {
+    	    		//para hacer las pruebas de si está bien la query después esto irá metido en un textarea para visualizar
+					datosTorneoPorTipo();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
     	    }
     	});
 	}
+	public void datosTorneoPorTipo() throws SQLException{
+		//habrá que hacer una query por cada tipo de torneo, por ahora hay 4 tipos (en caso de haber más se podría hacer con un while)
+		String query2="select count(case id_contest_type when 'contest_type_1' then 1 else NULL end ) as value from panenka.contests "
+				+ "where YEAR(open_date) = YEAR(CURDATE()) ;";
+		ResultSet jornadas= driverDB.runQuery(query2);
+		String queJornada=new String();
+		while(jornadas.next()){
+			queJornada=jornadas.getString("value");
+		}
+		System.out.println(queJornada);
+	}
 }
+
+
