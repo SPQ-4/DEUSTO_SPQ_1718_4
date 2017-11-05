46e9d97b55c0c21d35322074f22f39490387a85c
Paula.Urain <paulaue@opendeusto.es>
2017-11-05 21:10:22 +0100

    query hecha por cada tipo torneo, falta definir los tipos de torneo #DSPQ1718P4-40 add work 2h

--- ProyectoJavaFx/src/main/java/controllers/ControllerGeneral.java
+++ ProyectoJavaFx/src/main/java/controllers/ControllerGeneral.java
@@ -1,112 +1,50 @@
 package controllers;
 
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import db.MySQLDriver;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
+import javafx.scene.chart.PieChart;
+import javafx.scene.text.Text;
+
 
 public class ControllerGeneral {
-	@FXML
-	private Button actualState;
-	@FXML
-	private Button economicState;
-	@FXML
-	private Button userState;
-	@FXML
-	private Button exit;
-	@FXML
-	private Pane centralPane;
 	
 	private MySQLDriver driverDB;
+	@FXML
+	private PieChart contestsRatio;
+	
+	private PieChart.Data slice1;
+	private PieChart.Data slice2;
+	
 	
 	@FXML
-	public void initialize() {
+	public void initialize() throws SQLException {
 
 		driverDB= new MySQLDriver();
-		//todos los setOnAction es para darles funcionalidad a los botones del menú de navegación
-		actualState.setOnAction(new EventHandler<ActionEvent>() {
-    	    @Override public void handle(ActionEvent e) {
-    	    	centralPane.getChildren().clear();
-    	    	try {
-					centralPane.getChildren().add((Node) FXMLLoader.load(getClass().getResource("../views/sample.fxml")));
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-    	    }
-    	});
+		datosTorneoPorTipo();
 		
-		economicState.setOnAction(new EventHandler<ActionEvent>() {
-    	    @Override public void handle(ActionEvent e) {
-    	    	centralPane.getChildren().clear();
-    	    	try {
-					centralPane.getChildren().add((Node) FXMLLoader.load(getClass().getResource("../views/sample.fxml")));
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-    	    }
-    	});
-		
-		userState.setOnAction(new EventHandler<ActionEvent>() {
-    	    @Override public void handle(ActionEvent e) {
-    	    	centralPane.getChildren().clear();
-    	    	try {
-					centralPane.getChildren().add((Node) FXMLLoader.load(getClass().getResource("../views/User_Table.fxml")));
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-    	    }
-    	});
-		exit.setOnAction(new EventHandler<ActionEvent>() {
-    	    @Override public void handle(ActionEvent e) {
-    	    	Stage stage = (Stage) exit.getScene().getWindow();
-    	        // do what you have to do
-    	       // stage.close();
-    	    	try {
-    	    		//para hacer las pruebas de si está bien la query después esto irá metido en un textarea para visualizar
-					datosTorneoPorTipo();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-    	    }
-    	});
 	}
 	public void datosTorneoPorTipo() throws SQLException{
 		//habrá que hacer una query por cada tipo de torneo, por ahora hay 2 tipos (en caso de haber más se podría hacer con un while)
 		String query1="select count(case id_contest_type when 'contest_type_1' then 1 else NULL end ) as value from panenka.contests "
 				+ "where YEAR(open_date) = YEAR(CURDATE()) ;";
 		ResultSet type1= driverDB.runQuery(query1);
-		String type1Q=new String();
 		while(type1.next()){
-			type1Q=type1.getString("value");
-		}
-		System.out.println(type1Q);
+			slice1 = new PieChart.Data("Head-to-Head", Integer.parseInt(type1.getString("value")));
+		}		
 		
 		String query2="select count(case id_contest_type when 'contest_type_2' then 1 else NULL end ) as value from panenka.contests "
 				+ "where YEAR(open_date) = YEAR(CURDATE()) ;";
 		ResultSet type2= driverDB.runQuery(query2);
-		String type2Q=new String();
 		while(type2.next()){
-			type2Q=type2.getString("value");
+			slice2 = new PieChart.Data("Multiuser", Integer.parseInt(type2.getString("value")));
 		}
-		System.out.println(type2Q);
+		ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(slice1,slice2);
+		contestsRatio.setData(pieChartData);
 	}
 }
 
