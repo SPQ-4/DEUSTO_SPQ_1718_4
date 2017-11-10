f3855cb84429cf8f81b949a907b5c0e1b530d220
Paula.Urain <paulaue@opendeusto.es>
2017-11-10 18:42:40 +0100

    para merge

--- ProyectoJavaFx/src/main/java/controllers/ControllerGeneral.java
+++ ProyectoJavaFx/src/main/java/controllers/ControllerGeneral.java
@@ -22,6 +22,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import org.apache.log4j.Logger;
+
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
@@ -64,30 +67,28 @@ public class ControllerGeneral implements Initializable{
 	
 	private PieChart.Data slice1;
 	private PieChart.Data slice2;
+	static Logger logger = Logger.getLogger(ControllerGeneral.class);
 	
+	public ControllerGeneral(){
+		driverDB= new MySQLDriver();
+	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		logger.info("inicializando controller general");
 		driverDB= new MySQLDriver();	
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../views/UserTable.fxml"));
 		try {
-			monthKPI.getChildren().add((GridPane)fxmlLoader.load());
-		} catch (IOException e) {
+			obtainMonthKPI();
+		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.warn("falla el método obtainMonthKPI(), problema de SQL");
+			e1.printStackTrace();
 		}
-		ControllerGeneral controller;// = fxmlLoader.getController();
-			/*try {
-				controller.obtainMonthKPI();
-			} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			}
-	       controller.paintMonthKPI();*/
+		paintMonthKPI();
 		
 		try {
-			datosTorneoPorTipo();
+			contestsTypes();
 		} catch (SQLException a) {
-			// TODO Auto-generated catch block
+			logger.warn("falla el método datosTorneoPorTipo(), problema de SQL");
 			a.printStackTrace();
 		}
 		ArrayList<Integer> datos = new ArrayList();
@@ -97,16 +98,59 @@ public class ControllerGeneral implements Initializable{
 			 suma=suma + i +3;
 		 }	 
 		double media = suma/datos.size();
-		fxmlLoader = new FXMLLoader(getClass().getResource("../views/monthUsersChart.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../views/monthUsersChart.fxml"));
 		try {
 			monthUsersChart.getChildren().add((Pane)fxmlLoader.load());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			logger.fatal("problemas al cargar el fxml");
 			e.printStackTrace();
 		}
 		UsersChartController controllerA = fxmlLoader.getController();
-	       controllerA.cargarDatos(datos, media);
+	    controllerA.cargarDatos(datos, media);
 	   
+	}
+	public Double playedRate() throws SQLException{
+		String playedTournaments="select count(*) from panenka_db.contests_contest where MONTH(close_date) = MONTH(CURDATE()) AND MONTH(created_date) = MONTH(CURDATE()) ;";
+		String openTournaments="select count(*) from panenka_db.contests_contest where MONTH(created_date) =MONTH(CURDATE())";
+		ResultSet type1=driverDB.runQuery(playedTournaments);
+		ResultSet type2=driverDB.runQuery(openTournaments);
+		Double playedRateParcial=new Double(0);
+		while(type1.next()){
+			 playedRateParcial=(double) (type1.getInt(1));
+		}	
+		while(type2.next()){
+			 playedRateParcial=playedRateParcial/(double)(type2.getInt(1));
+		}	
+		return playedRateParcial;
+	}
+	public Double revenuesByMonth() throws SQLException{
+		String revenues="select sum(\"fee\") from panenka_db.contests_contest,panenka_db.contests_entry where MONTH(created_date) =MONTH(CURDATE()) AND panenka_db.contests_entry.id_contest_id=panenka_db.contests_contest.id";
+		ResultSet type3=driverDB.runQuery(revenues);
+		Double revenuesThisMonthParcial=new Double(0);
+		if(type3!=null) {
+			while(type3.next()) {
+				revenuesThisMonthParcial=(double)type3.getInt(1);
+			}
+		}
+		return revenuesThisMonthParcial;
+	}
+	public Double byUsers() throws SQLException{
+		String by_admin="SELECT count(*) FROM panenka_db.contests_contest where created_by_admin=0";
+		ResultSet type4=driverDB.runQuery(by_admin);
+		Double tournamentsByUsersParcial=new Double(0);
+		while(type4.next()) {
+			tournamentsByUsersParcial=(double)type4.getInt(1);
+		}
+		return tournamentsByUsersParcial;
+	}
+	public Double usersMonth() throws SQLException{
+		String usersMonth="SELECT count(DISTINCT \"user_id\") FROM panenka_db.users_login where MONTH(login_date)=MONTH(CURDATE())";
+		ResultSet type5=driverDB.runQuery(usersMonth);
+		Double usersThisMonthParcial=new Double(0);
+		while(type5.next()) {
+			usersThisMonthParcial=(double)type5.getInt(1);
+		}
+		return usersThisMonthParcial;
 	}
 	public void paintMonthKPI() {
 		double playedRateD=Double.parseDouble(playedRate.getText());
@@ -147,57 +191,28 @@ public class ControllerGeneral implements Initializable{
 	}
 	public void obtainMonthKPI() throws SQLException {
 		//OBTENER DE LA BASE DE DATOS
-		String playedTournaments="select count(*) from panenka_db.contests_contest where MONTH(close_date) = MONTH(CURDATE()) AND MONTH(created_date) = MONTH(CURDATE()) ;";
-		String openTournaments="select count(*) from panenka_db.contests_contest where MONTH(created_date) =MONTH(CURDATE())";
-		String revenues="select sum(\"fee\") from panenka_db.contests_contest,panenka_db.contests_entry where MONTH(created_date) =MONTH(CURDATE()) AND panenka_db.contests_entry.id_contest_id=panenka_db.contests_contest.id";
-		String by_admin="SELECT count(*) FROM panenka_db.contests_contest where created_by_admin=0";
-		String usersMonth="SELECT count(DISTINCT \"user_id\") FROM panenka_db.users_login where MONTH(login_date)=MONTH(CURDATE())";
-		ResultSet type1=driverDB.runQuery(playedTournaments);
-		ResultSet type2=driverDB.runQuery(openTournaments);
-		ResultSet type3=driverDB.runQuery(revenues);
-		ResultSet type4=driverDB.runQuery(by_admin);
-		ResultSet type5=driverDB.runQuery(usersMonth);
-		Double playedRateParcial=new Double(0);
-		Double revenuesThisMonthParcial=new Double(0);
-		Double tournamentsByUsersParcial=new Double(0);
-		Double usersThisMonthParcial=new Double(0);
-		while(type1.next()){
-			 playedRateParcial=(double) (type1.getInt(1));
-		}	
-		while(type2.next()){
-			 playedRateParcial=playedRateParcial/(double)(type2.getInt(1));
-		}	
-		if(type3!=null) {
-			while(type3.next()) {
-				revenuesThisMonthParcial=(double)type3.getInt(1);
-			}
-		}
-		while(type4.next()) {
-			tournamentsByUsersParcial=(double)type4.getInt(1);
-		}
-		while(type5.next()) {
-			usersThisMonthParcial=(double)type5.getInt(1);
-		}
-		this.playedRate.setText(playedRateParcial.toString());
-		this.revenuesThisMonth.setText(revenuesThisMonthParcial.toString());
-		this.tournamentsByUsers.setText(tournamentsByUsersParcial.toString());
-		this.usersThisMonth.setText(usersThisMonthParcial.toString());
+		//String playedTournaments="select count(*) from panenka_db.contests_contest where MONTH(close_date) = MONTH(CURDATE()) AND MONTH(created_date) = MONTH(CURDATE()) ;";
+		//String openTournaments="select count(*) from panenka_db.contests_contest where MONTH(created_date) =MONTH(CURDATE())";
+		this.playedRate.setText(playedRate().toString());
+		this.revenuesThisMonth.setText(revenuesByMonth().toString());
+		this.tournamentsByUsers.setText(byUsers().toString());
+		this.usersThisMonth.setText(usersMonth().toString());
 	}
-	public void datosTorneoPorTipo() throws SQLException{
-		//habrá que hacer una query por cada tipo de torneo, por ahora hay 2 tipos (en caso de haber más se podría hacer con un while)
-		String query1="select count(case contest_type_id when 1 then 1 else NULL end ) as value from panenka_db.contests_contest "
+	public Double getValueContestType(int a) throws NumberFormatException, SQLException{
+		double value=0;
+		String query1="select count(case contest_type_id when'"+a+"' then 1 else NULL end ) as value from panenka_db.contests_contest "
 				+ "where YEAR(close_date) = YEAR(CURDATE()) ;";
 		ResultSet type1= driverDB.runQuery(query1);
 		while(type1.next()){
-			slice1 = new PieChart.Data("Public", Integer.parseInt(type1.getString("value")));
+			value = Integer.parseInt(type1.getString("value"));
 		}		
+		return value;
+	}
+	public void contestsTypes() throws SQLException{
+		//habrá que hacer una query por cada tipo de torneo, por ahora hay 2 tipos (en caso de haber más se podría hacer con un while)
 		
-		String query2="select count(case contest_type_id when '2' then 1 else NULL end ) as value from panenka_db.contests_contest "
-				+ "where YEAR(close_date) = YEAR(CURDATE()) ;"; 
-		ResultSet type2= driverDB.runQuery(query2);
-		while(type2.next()){
-			slice2 = new PieChart.Data("Private", Integer.parseInt(type2.getString("value")));
-		}
+			slice1 = new PieChart.Data("Public", getValueContestType(1));		
+			slice2 = new PieChart.Data("Private", getValueContestType(2));
 		ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(slice1,slice2);
 		contestsRatio.setData(pieChartData);
 		caption.setTextFill(Color.BLACK);
@@ -209,9 +224,7 @@ public class ControllerGeneral implements Initializable{
 		                	caption.setLayoutX(e.getSceneX());
 		                    caption.setLayoutY(e.getSceneY());
 		                    caption.setText(Double.toString(data.getPieValue()));
-		                    System.out.println(e.getSceneX());
-		                    System.out.println(e.getX());
-			             }
+		                  }
 			        });
 		}
 	}
